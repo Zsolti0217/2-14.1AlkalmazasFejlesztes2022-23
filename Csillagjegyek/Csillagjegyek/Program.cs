c8cd32e5741ef1288cf3d77610e46ad919ed973d
V.Zsolt <90308512+Zsolti0217@users.noreply.github.com>
2022-09-12 22:27:11 +0200

    Csillagjegyek

---
+++ Csillagjegyek/Csillagjegyek/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csillagjegyek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine( "Írd be a hónap számát");
+
+            int honapszam, honaphiba = 0, napszam, naphiba = 0, napok;
+            int[] honapok = new int[] { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+            DateTime kos = new DateTime(2022, 3, 21);
+            DateTime bika = new DateTime(2022, 4, 21);
+            DateTime ikrek = new DateTime(2022, 5, 22);
+            DateTime rak = new DateTime(2022, 6, 22);
+            DateTime oroszlan = new DateTime(2022, 7, 23);
+            DateTime szuz = new DateTime(2022, 8, 24);
+            DateTime merleg = new DateTime(2022, 9, 23);
+            DateTime skorpio = new DateTime(2022, 10, 24);
+            DateTime nyilas = new DateTime(2022, 11, 23);
+            DateTime bak = new DateTime(2022, 12, 22);
+            DateTime vizonto = new DateTime(2023, 1, 21);
+            DateTime halak = new DateTime(2023, 2, 20);
+
+            string csillagjegy = "";
+            do
+            {
+                if (honaphiba > 0)
+                {
+                    Console.WriteLine("Helyes adatokat írj be! A hónapok száma 1-12 lehet!\nÍrd be újra a számot!");
+                }
+                honaphiba++;
+            } while (!int.TryParse(Console.ReadLine(), out honapszam) || honapszam > 12 || honapszam < 1);
+            Console.WriteLine("Add meg a napok számát!");
+
+            do
+            {
+                napok = honapok[honapszam - 1];
+                if (naphiba > 0)
+                {
+                    Console.WriteLine("Helyes adatokat írj be!\nPróbáld újra!");
+                }
+                naphiba++;
+            } while (!int.TryParse(Console.ReadLine(), out napszam) || napszam > napok || napszam < 1);
+            DateTime datum = new DateTime(2022, honapszam, napszam);
+
+            if (datum >= kos && datum < bika) { csillagjegy = "Kos"; }
+            else if (datum >= bika && datum <= ikrek) { csillagjegy = "Bika"; }
+            else if (datum >= ikrek && datum <= rak) { csillagjegy = "Ikrek"; }
+            else if (datum >= rak && datum <= oroszlan) { csillagjegy = "Rák"; }
+            else if (datum >= oroszlan && datum <= szuz) { csillagjegy = "Oroszlán"; }
+            else if (datum >= szuz && datum <= merleg) { csillagjegy = "Szűz"; }
+            else if (datum >= merleg && datum <= skorpio) { csillagjegy = "Mérleg"; }
+            else if (datum >= skorpio && datum <= nyilas) { csillagjegy = "Skorpió"; }
+            else if (datum >= nyilas && datum <= bak) { csillagjegy = "Nyilas"; }
+            else if (datum >= bak && datum <= vizonto) { csillagjegy = "Bak"; }
+            else if (datum >= vizonto && datum <= halak) { csillagjegy = "Vizöntő"; }
+            else if (datum >= halak) { csillagjegy = "Halak"; }
+
+            Console.WriteLine($"A csillagjegyed: {csillagjegy} ");
+            Console.ReadKey();
+        }
+    }
+}
