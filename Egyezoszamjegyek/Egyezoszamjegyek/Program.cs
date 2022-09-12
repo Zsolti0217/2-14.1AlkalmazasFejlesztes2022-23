72bbdc449392e1aea43b9ef946d57b42ce564977
V.Zsolt <90308512+Zsolti0217@users.noreply.github.com>
2022-09-12 21:50:53 +0200

    Egyezoszamjegyek

---
+++ Egyezoszamjegyek/Egyezoszamjegyek/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Egyezoszamjegyek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] szamsor = new double[2];
+            double szam1, szam2;
+
+            do
+            {
+                Console.WriteLine("Adj meg egy számot");
+                szam1 = double.Parse(Console.ReadLine());
+                szamsor[0] = szam1;
+
+                Console.WriteLine("Adj meg még egy számot");
+                szam2 = double.Parse(Console.ReadLine());
+                szamsor[1] = szam2;
+
+            } while (szamsor[0] != szamsor[1] );
+
+            Console.WriteLine("Utolsó két szám megegyezik!");
+
+            foreach (var index in szamsor)
+            {
+                Console.WriteLine(index);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
