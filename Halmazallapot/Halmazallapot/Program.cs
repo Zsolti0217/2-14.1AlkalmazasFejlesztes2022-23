fba78a464c03d9a2d80890e622677cebc3e83d5d
V.Zsolt <90308512+Zsolti0217@users.noreply.github.com>
2022-09-12 21:36:35 +0200

    Halmazallapot
    Teams Feladat 2022.09.12

---
+++ Halmazallapot/Halmazallapot/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Halmazallapot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double homerseklet;
+            string halmazallapot = ""; 
+            int hibak = 0;
+            Console.WriteLine( "Írja be a víz hőmérsékletét" );
+
+            do
+            {
+                if (hibak > 1)
+
+                {Console.WriteLine( "Kérlek helyesen add meg az adatokat! Csak számot fogadok el!\nPróbád újra"); }
+
+                hibak++;
+
+            } while (!double.TryParse(Console.ReadLine(), out homerseklet));
+
+
+            if (homerseklet < 0)
+            {
+                halmazallapot = "Jég";
+            }
+            else if (homerseklet > 100)
+            {
+                halmazallapot = "Gőz";
+            }
+            else
+            {
+                halmazallapot = "Folyékony";
+            }
+
+            Console.WriteLine($"Az adatok alapján a víz halmazállapota: {halmazallapot}\nHőmérséklete: {homerseklet}°C");
+            Console.ReadKey();
+        }
+    }
+}
