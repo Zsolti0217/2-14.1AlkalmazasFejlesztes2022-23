d8e8589610e14000b0b4c8b45ca67a536564d856
V.Zsolt <90308512+Zsolti0217@users.noreply.github.com>
2022-09-13 12:12:52 +0200

    GitHub teszt

---
+++ Program/Program/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        //Állítsunk elő véletlenszerűen 40 egész számot
+        //a [-100,100]-ból és írjuk ki a képernyőre egymás mellé!
+        //A kiírás végén adjuk meg hány darab pozitív szám van! 
+
+        {
+            Random random = new Random();
+
+            for (int i = 0; i <= 40; i++)
+            {
+                Console.WriteLine($"{random.Next(-100,100)}");
+            }
+            Console.ReadKey();
+            
+            
+
+           
+        }
+    }
+}
